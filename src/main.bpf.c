1ef78c7f4ec501e406b2890bd53a278420c78ad0
hitori <hitori1403@gmail.com>
2024-12-04 18:40:00 +0700

    handle full process path in rules checking

--- src/main.bpf.c
+++ src/main.bpf.c
@@ -55,6 +55,12 @@ const volatile int loader_pid = 0;
 
 char path_buf[PATH_MAX];
 
+// NOTE: Using smaller size in newer kernel version if possible
+/* char process_path[PATH_MAX + NAME_MAX]; */
+char proc_path[65536];
+
+int proc_path_mtx = 0;
+
 struct cb_pathcmp_ctx {
 	char *s1;
 	char *s2;
@@ -76,7 +82,67 @@ static int cb_pathcmp(u32 i, struct cb_pathcmp_ctx *ctx)
 		ctx->result = 0;
 		return 1;
 	}
+
 	return 0;
+}
+
+struct cb_strrev_ctx {
+	char *s;
+	u16 pos;
+	u16 len;
+};
+
+static int cb_strrev(u32 idx, struct cb_strrev_ctx *ctx)
+{
+	u16 left = ctx->pos + idx;
+	u16 right = ctx->pos + ctx->len - idx - 1;
+
+	if (left >= PATH_MAX || right >= PATH_MAX)
+		return 1;
+
+	u8 tmp = ctx->s[left];
+	ctx->s[left] = ctx->s[right];
+	ctx->s[right] = tmp;
+
+	return 0;
+}
+
+static int get_d_path(char *buf, struct task_struct *task)
+{
+	char *name;
+	u16 buf_len = 0;
+	struct dentry *dentry = BPF_CORE_READ(task, mm, exe_file, f_path.dentry);
+
+	for (u32 i = 0; i < PATH_MAX / 2; ++i) {
+		bpf_core_read(&name, sizeof(name), &dentry->d_name.name);
+
+		if (buf_len >= PATH_MAX - 1)
+			break;
+
+		u16 len = bpf_probe_read_kernel_str(&buf[buf_len], NAME_MAX, name) - 1;
+
+		if (buf_len >= PATH_MAX || buf[buf_len] == '/') {
+			buf[buf_len] = 0; // remove last slash
+			break;
+		}
+
+		struct cb_strrev_ctx cb_ctx = { buf, buf_len, len };
+		bpf_loop(len / 2, (void *)cb_strrev, &cb_ctx, 0);
+
+		buf_len += len;
+
+		if (buf_len < PATH_MAX)
+			buf[buf_len] = '/';
+
+		++buf_len;
+
+		dentry = BPF_CORE_READ(dentry, d_parent);
+	}
+
+	struct cb_strrev_ctx cb_ctx = { buf, 0, buf_len };
+	bpf_loop(buf_len / 2, (void *)cb_strrev, &cb_ctx, 0);
+
+	return buf_len;
 }
 
 SEC("tp/syscalls/sys_enter_openat")
@@ -84,6 +150,9 @@ int handle_enter_openat(struct trace_event_raw_sys_enter *ctx)
 {
 	u32 pid = bpf_get_current_pid_tgid();
 	if (pid == loader_pid)
+		return 0;
+
+	if (!proc_path_mtx)
 		return 0;
 
 	// BUG: https://github.com/iovisor/bcc/issues/3175
@@ -102,17 +171,12 @@ int handle_enter_openat(struct trace_event_raw_sys_enter *ctx)
 
 	struct task_struct *task = (struct task_struct *)bpf_get_current_task();
 
-	// TODO: handle full path
-	const char *name =
-		(const char *)BPF_CORE_READ(task, mm, exe_file, f_path.dentry, d_name.name);
-	u32 name_len = bpf_probe_read_kernel_str(path_buf, sizeof(path_buf), name);
-
 	for (u32 i = 0; i < MAX_PROCESSES_PER_FILE; ++i) {
 		if (!procs[i].path[0]) {
 			break;
 		}
 
-		struct cb_pathcmp_ctx cb_ctx = { (char *)procs[i].path, path_buf, 0 };
+		struct cb_pathcmp_ctx cb_ctx = { (char *)procs[i].path, proc_path, 0 };
 		bpf_loop(PATH_MAX, cb_pathcmp, &cb_ctx, 0);
 
 		if (cb_ctx.result)
@@ -122,11 +186,39 @@ int handle_enter_openat(struct trace_event_raw_sys_enter *ctx)
 		u64 zero = 0;
 
 		bpf_map_update_elem(&map_fd_offset, &pid_fd, &zero, BPF_ANY);
+		proc_path_mtx = 0;
 
 		return 0;
 	}
 
-	bpf_send_signal(9); // SIGKILL
+	// TODO: handle SIGKILL
+	/* bpf_send_signal(9); // SIGKILL */
+	proc_path_mtx = 0;
+
+	return 0;
+}
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PROG_ARRAY);
+	__uint(max_entries, 2);
+	__uint(key_size, sizeof(u32));
+	__array(values, int(void *));
+} map_progs SEC(".maps") = {
+	.values = {
+		[1] = (void *)&handle_enter_openat,
+	},
+};
+
+SEC("tp/syscalls/sys_enter_openat")
+int get_proc_path(struct trace_event_raw_sys_enter *ctx)
+{
+	if (proc_path_mtx)
+		return 0;
+
+	struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+	get_d_path(proc_path, task);
+	proc_path_mtx = 1;
+	bpf_tail_call(ctx, &map_progs, 1);
 
 	return 0;
 }
